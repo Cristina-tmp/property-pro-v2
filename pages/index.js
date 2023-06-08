d64210b37a0c3c0aba59f476c3876cd0e470263e
Cristina-tmp <cristinacommissions@gmail.com>
2023-06-08 18:30:53 -0400

    finished Header component

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,19 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import Head from "next/head";
+import Header from "../components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <div className="">hi</div>;
+  return (
+    <div className="">
+      <Head>
+        <title>Property Pro</title>
+      </Head>
+      {/* header */}
+      <Header />
+      {/* banner */}
+    </div>
+  );
 }
